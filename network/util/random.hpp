531a4331830bda4343cc30c6ec280d2185e41509
hbccdf <hbccdf@126.com>
2019-11-05 22:45:55 +0800

    add random utils

--- network/util/random.hpp
+++ network/util/random.hpp
@@ -13,7 +13,44 @@ namespace cytx
 
             class random_util
             {
+            public:
+                //range [0.0f, 1.0f]
+                static float get_float()
+                {
+                    int ret = get_int(100);
+                    return ret / 100.0f;
+                }
+                //range [begin_value, end_value]
+                static int get_int(int begin_value, int end_value)
+                {
+                    std::uniform_int_distribution<int> distribution(begin_value, end_value);
+                    int ret = distribution(get_engine());
+                    return ret;
+                }
+                //range [0, end_value]
+                static int get_int(int end_value)
+                {
+                    return get_int(0, end_value);
+                }
 
+            public:
+                static bool hit(int chance, int total)
+                {
+                    return chance < get_int(total);
+                }
+                static bool hit(int chance)
+                {
+                    return hit(chance, _base_chance);
+                }
+
+                static bool miss(int chance, int total)
+                {
+                    return chance > get_int(total);
+                }
+                static bool miss(int chance)
+                {
+                    return miss(chance, _base_chance);
+                }
             private:
                 static random_engine_t& get_engine()
                 {
@@ -25,7 +62,11 @@ namespace cytx
                     return *generator;
                 }
 
+                static const int _base_chance = 10000;
+
             };
         }
+
+        using random_util = detail::random_util;
     }
 }
---
+++ network/util/random_string.hpp
@@ -0,0 +1,99 @@
+#pragma once
+#include "random.hpp"
+#include <string>
+#include <fmt/format.h>
+#include <boost/algorithm/string.hpp>
+#include "cast.hpp"
+
+namespace cytx
+{
+    namespace util
+    {
+        using memory_wirter_t = fmt::MemoryWriter;
+        class random_string
+        {
+        public:
+            //get a fixed length string with random content, contain number upper and lower letters
+            static std::string get_string(int length)
+            {
+                return get_string_with_chars(length, ins().all_char_);
+            }
+
+            static std::string get_letter_string(int length)
+            {
+                return get_string_with_chars(length, ins().letter_char_);
+            }
+
+            static std::string get_upper_string(int length)
+            {
+                std::string str = get_letter_string(length);
+                boost::to_upper(str);
+                return str;
+            }
+
+            static std::string get_lower_string(int length)
+            {
+                std::string str = get_letter_string(length);
+                boost::to_lower(str);
+                return str;
+            }
+
+            static std::string get_number_string(int length)
+            {
+                return get_string_with_chars(length, ins().number_char_);
+            }
+
+            static std::string get_string_with_chars(int length, const std::string& chars)
+            {
+                memory_wirter_t mr;
+                if (!chars.empty())
+                {
+                    for (int i = 0; i < length; ++i)
+                    {
+                        int index = random_util::get_int((int)chars.length() - 1);
+                        char c = chars[index];
+                        mr << c;
+                    }
+                }
+                return mr.str();
+            }
+
+            static std::string get_zero_string(int length)
+            {
+                memory_wirter_t mr;
+                for (int i = 0; i < length; ++i)
+                {
+                    mr << '0';
+                }
+                return mr.str();
+            }
+
+            //Generate a string of fixed length based on the number, which is not long enough to fill in 0
+            //not support less than zero number
+            static std::string get_fixed_length_string(int64_t number, int fixed_length)
+            {
+                memory_wirter_t mr;
+                std::string num_str = cast_string(number);
+                mr << get_zero_string(fixed_length - (int)num_str.length());
+                mr << number;
+                return mr.str();
+            }
+
+            static std::string get_token()
+            {
+                return "";
+            }
+
+        private:
+            static random_string& ins()
+            {
+                static random_string r;
+                return r;
+            }
+
+            const std::string all_char_ = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const std::string letter_char_ = "abcdefghijkllmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const std::string number_char_ = "0123456789";
+        };
+    }
+}
