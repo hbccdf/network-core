652b9194d1dcec6280844fbc2cf8c449336730d5
xing.wang <xing.wang@cytxcn.com>
2019-11-11 19:43:40 +0800

    modify option helper

--- network/cmder/cmder_helper.hpp
+++ network/cmder/cmder_helper.hpp
@@ -8,69 +8,106 @@ namespace cytx
     using init_parser_func_t = std::function<void(bpo_parser_t&, bpo_options_t&, bpo_pos_options_t*)>;
 
     class base_cmder;
-    class add_options_helper
+
+    template<typename T>
+    class base_options_helper
     {
+        using real_t = T;
         friend class base_cmder;
-    private:
-        add_options_helper(bpo_parser_t& de, bpo_options_t* op, bpo_pos_options_t* pod, init_parser_func_t func)
+    public:
+        base_options_helper(bpo_parser_t* de, bpo_options_t* op, bpo_pos_options_t* pod)
             : de_(de)
             , op_(op)
             , pod_(pod)
+        {
+
+        }
+
+        base_options_helper(base_options_helper&& other)
+            : de_(other.de_)
+            , op_(other.op)
+            , pod_(other.pod)
+        {
+        }
+
+        real_t& operator()(const char* name, const char* desc)
+        {
+            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, new bpo::untyped_value(true), desc));
+            op_->add(d);
+            return get_real_value();
+        }
+
+        real_t& operator()(const char* name, const bpo::value_semantic* s)
+        {
+            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, s));
+            op_->add(d);
+            return get_real_value();
+        }
+
+        real_t& operator()(const char* name, const bpo::value_semantic* s, const char* desc)
+        {
+            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, s, desc));
+            op_->add(d);
+            return get_real_value();
+        }
+
+        real_t& operator()(const char* name, const char* desc, int max_count)
+        {
+            set_pos(name, max_count);
+            return operator()(name, desc);
+        }
+
+        real_t& operator()(const char* name, const bpo::value_semantic* s, int max_count)
+        {
+            set_pos(name, max_count);
+            return operator()(name, s);
+        }
+
+        real_t& operator()(const char* name, const bpo::value_semantic* s, const char* desc, int max_count)
+        {
+            set_pos(name, max_count);
+            return operator()(name, s, desc);
+        }
+
+    protected:
+        void set_pos(const char* name, int max_count)
+        {
+            pod_->add(name, max_count);
+        }
+
+        T& get_real_value()
+        {
+            return *reinterpret_cast<T*>(this);
+        }
+
+    protected:
+        bpo_parser_t* de_;
+        bpo_options_t* op_;
+        bpo_pos_options_t* pod_;
+    };
+
+    class add_options_helper : public base_options_helper<add_options_helper>
+    {
+        friend class base_cmder;
+        using base_t = base_options_helper<add_options_helper>;
+    private:
+        add_options_helper(bpo_parser_t& de, bpo_options_t* op, bpo_pos_options_t* pod, init_parser_func_t func)
+            : base_t(&de, op, pod)
             , func_(func)
         {
 
         }
     public:
-        add_options_helper(const add_options_helper& other)
-            : de_(other.de_)
-            , op_(other.op_)
-            , pod_(other.pod_)
+        add_options_helper(add_options_helper&& other)
+            : base_t(other.de_, other.op_, other.pod_)
+            , func_(std::move(other.func_))
         {
 
         }
 
         ~add_options_helper()
         {
-            func_(de_, *op_, pod_);
-        }
-
-        add_options_helper& operator()(const char* name, const char* desc)
-        {
-            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, new bpo::untyped_value(true), desc));
-            op_->add(d);
-            return *this;
-        }
-
-        add_options_helper& operator()(const char* name, const bpo::value_semantic* s)
-        {
-            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, s));
-            op_->add(d);
-            return *this;
-        }
-
-        add_options_helper& operator()(const char* name, const bpo::value_semantic* s, const char* desc)
-        {
-            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, s, desc));
-            op_->add(d);
-            return *this;
-        }
-
-        add_options_helper& operator()(const char* name, const char* desc, int max_count)
-        {
-            set_pos(name, max_count);
-            return operator()(name, desc);
-        }
-
-        add_options_helper& operator()(const char* name, const bpo::value_semantic* s, int max_count)
-        {
-            set_pos(name, max_count);
-            return operator()(name, s);
-        }
-
-        add_options_helper& operator()(const char* name, const bpo::value_semantic* s, const char* desc, int max_count)
-        {
-            set_pos(name, max_count);
-            return operator()(name, s, desc);
+            func_(*de_, *op_, pod_);
         }
 
         int end() const
@@ -79,73 +116,28 @@ namespace cytx
         }
 
     private:
-        void set_pos(const char* name, int max_count)
-        {
-            pod_->add(name, max_count);
-        }
-
-    private:
-        bpo_parser_t& de_;
-        bpo_options_t* op_;
-        bpo_pos_options_t* pod_;
         init_parser_func_t func_;
     };
 
-    class cmder_helper
+    class cmder_helper : public base_options_helper<cmder_helper>
     {
+        using base_t = base_options_helper<cmder_helper>;
     public:
         cmder_helper(const std::string& name, const std::string& desc)
-            : name_(name)
+            : base_t(&de_, &op_, &pod_)
+            , name_(name)
             , desc_(desc)
         {
             de_.enum_with_str(true);
         }
 
         cmder_helper(cmder_helper&& other)
-            : name_(std::move(other.name_))
+            : base_t(&de_, &op_, &pod_)
+            , name_(std::move(other.name_))
             , desc_(std::move(other.desc_))
             , op_(std::move(other.op_))
             , pod_(std::move(other.pod_))
         {}
-
-        cmder_helper& operator()(const char* name, const char* desc)
-        {
-            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, new bpo::untyped_value(true), desc));
-            op_.add(d);
-            return *this;
-        }
-
-        cmder_helper& operator()(const char* name, const bpo::value_semantic* s)
-        {
-            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, s));
-            op_.add(d);
-            return *this;
-        }
-
-        cmder_helper& operator()(const char* name, const bpo::value_semantic* s, const char* desc)
-        {
-            boost::shared_ptr<bpo_option_t> d(new bpo_option_t(name, s, desc));
-            op_.add(d);
-            return *this;
-        }
-
-        cmder_helper& operator()(const char* name, const char* desc, int max_count)
-        {
-            set_pos(name, max_count);
-            return operator()(name, desc);
-        }
-
-        cmder_helper& operator()(const char* name, const bpo::value_semantic* s, int max_count)
-        {
-            set_pos(name, max_count);
-            return operator()(name, s);
-        }
-
-        cmder_helper& operator()(const char* name, const bpo::value_semantic* s, const char* desc, int max_count)
-        {
-            set_pos(name, max_count);
-            return operator()(name, s, desc);
-        }
 
     public:
         template<typename T>
@@ -160,11 +152,6 @@ namespace cytx
             return t;
         }
 
-    private:
-        void set_pos(const char* name, int max_count)
-        {
-            pod_.add(name, max_count);
-        }
     private:
         bpo_parser_t de_;
         std::string name_;
