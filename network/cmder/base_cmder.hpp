7391bbad1af882dbd66fdb35d3c2a2649a24ac2b
xing.wang <hbccdf@126.com>
2019-11-27 00:58:15 +0800

    add cmder meta

--- network/cmder/base_cmder.hpp
+++ network/cmder/base_cmder.hpp
@@ -80,36 +80,4 @@ namespace cytx
         parser_func_t parser_func_;
         init_parser_func_t init_parser_func_;
     };
-
-    class cmder_factory
-    {
-    public:
-        static cmder_factory& ins()
-        {
-            static cmder_factory factory;
-            return factory;
-        }
-
-        template<typename T>
-        auto register_cmder(const char* name, const char* desc) -> std::enable_if_t<std::is_base_of<base_cmder, T>::value, add_options_helper>
-        {
-            auto cmder = instance_factory::ins().get<T>();
-            icmder_manager::ins().register_cmder(name, cmder);
-
-            cmder->set_parser([cmder](auto& de) {
-                de.DeSerialize(*cmder);
-            });
-
-            cmder->set_init_parser([cmder](auto& de, auto& ops, auto pos_ops) {
-                de.init<T>(*cmder, ops, pos_ops);
-            });
-
-            cmder->init_options(name, desc);
-            return cmder->add_options();
-        }
-    };
-
-#define REG_CMDER(type, name, desc) \
-REG_TYPE(name); \
-static int __REGISTER_CMDER_VAL__ ## type ## __LINE__ = (cmder_factory::ins().register_cmder<type>(name, desc))
 }
--- network/module/module_manager.hpp
+++ network/module/module_manager.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include "base_module.hpp"
 #include "network/base/world.hpp"
+#include "network/meta/type_meta.hpp"
 
 namespace cytx
 {
@@ -83,6 +84,7 @@ namespace cytx
 }
 
 #define REG_MODULE(type)                                                                        \
+REG_TYPE(type)                                                                                  \
 namespace __reg_module_ ## type ## __LINE__                                                     \
 {                                                                                               \
     static int r = (cytx::module_manager::ins().register_module<type>(#type), 0);               \
