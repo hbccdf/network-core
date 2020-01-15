226f08a6790f01240f6cfcc0aa9c2a69bcdb99b7
xing.wang <xing.wang@cytxcn.com>
2020-01-15 15:31:42 +0800

    fix bug of random

--- network/util/random.hpp
+++ network/util/random.hpp
@@ -7,66 +7,63 @@ namespace cytx
 {
     namespace util
     {
-        namespace detail
+        class random_util
         {
             using random_engine_t = std::mt19937;
-
-            class random_util
+        public:
+            //range [0.0f, 1.0f]
+            static float get_float()
             {
-            public:
-                //range [0.0f, 1.0f]
-                static float get_float()
-                {
-                    int ret = get_int(100);
-                    return ret / 100.0f;
-                }
-                //range [begin_value, end_value]
-                static int get_int(int begin_value, int end_value)
-                {
-                    std::uniform_int_distribution<int> distribution(begin_value, end_value);
-                    int ret = distribution(get_engine());
-                    return ret;
-                }
-                //range [0, end_value]
-                static int get_int(int end_value)
-                {
-                    return get_int(0, end_value);
-                }
+                int ret = get_int(100);
+                return ret / 100.0f;
+            }
+            //range [begin_value, end_value]
+            static int get_int(int begin_value, int end_value)
+            {
+                std::uniform_int_distribution<int> distribution(begin_value, end_value);
+                int ret = distribution(get_engine());
+                return ret;
+            }
+            //range [0, end_value]
+            static int get_int(int end_value)
+            {
+                return get_int(0, end_value);
+            }
 
-            public:
-                static bool hit(int chance, int total)
-                {
-                    return chance < get_int(total);
-                }
-                static bool hit(int chance)
-                {
-                    return hit(chance, _base_chance);
-                }
+        public:
+            static bool hit(int chance, int total)
+            {
+                return chance > get_int(total);
+            }
+            static bool hit(int chance)
+            {
+                return hit(chance, _base_chance);
+            }
 
-                static bool miss(int chance, int total)
+            static bool miss(int chance, int total)
+            {
+                return chance < get_int(total);
+            }
+            static bool miss(int chance)
+            {
+                return miss(chance, _base_chance);
+            }
+        private:
+            static random_engine_t& get_engine()
+            {
+                static thread_local random_engine_t* generator = nullptr;
+                if (!generator)
                 {
-                    return chance > get_int(total);
+                    uint32_t seed = clock() + GetCurrentThreadId();
+                    generator = new random_engine_t(seed);
                 }
-                static bool miss(int chance)
-                {
-                    return miss(chance, _base_chance);
-                }
-            private:
-                static random_engine_t& get_engine()
-                {
-                    static thread_local random_engine_t* generator = nullptr;
-                    if (!generator)
-                    {
-                        generator = new random_engine_t(clock());
-                    }
-                    return *generator;
-                }
+                return *generator;
+            }
 
-                static const int _base_chance = 10000;
+            static const int _base_chance = 10000;
 
-            };
-        }
-
-        using random_util = detail::random_util;
+        };
     }
+
+    using random_util = util::random_util;
 }
